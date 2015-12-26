b23c3b174833470b0a9cca10b1ca300dbd92a3c9
BJSoft <qjawns0117@naver.com>
2015-12-26 22:07:35 +0900

    new (error)

--- chat.py
+++ chat.py
@@ -1,22 +1,65 @@
 import socket
 import sys
 import os
+import thread
 
+'''class ThreadClass(threading.Thread):
+	def serverdata(sck, adr):
+		while True:
+			data = sck.recv(1024)
+			if not data: break
+			sck.sendall(data)
+
+		sck.close()'''
+
+def serverdata(sck, adr):
+	data = sck.recv(1024)
+	#if not data: break
+	#sck.sendall(data)
+	print data
+
+def serversend(sck, adr):
+	msg = raw_input('msg >')
+	try:
+		if msg == '/exit':
+			sck.close()
+		sck.send(msg)
+	except:
+		sck.close()
+
+def clientsend(sck, cs):
+	msg = raw_input('msg >')
+	try:
+		if msg == '/exit':
+			sck.close()
+		sck.send(msg)
+	except:
+		sck.close()
+
+def clientdata(sck, cs):
+	data = sck.recv(1024)
+	#if not data: break
+	#sck.sendall(data)
+	print data
 
 def client(HOST, PORT):
 	os.system("clear")
 	cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	cs.connect((HOST, PORT))
+	
 	while True:
 		msg = raw_input('msg >')
 		try:
 			if msg == '/exit':
 				cs.close()
 			cs.send(msg)
+
+			data = cs.recv(1024)
+			if not data: break
+			cs.sendall(data)
+			print data			
 		except:
 			cs.close()
-	
-	cs.close()
 
 
 def server(host, port):
@@ -28,23 +71,44 @@ def server(host, port):
   \___ \| __/ _` | '__| __|  / __| '_ \ / _` | __|
   ____) | || (_| | |  | |_  | (__| | | | (_| | |_
  |_____/ \__\__,_|_|   \__|  \___|_| |_|\__,_|\__|
-
-
                                                 	"""
 	print "%s"% socket.gethostbyname(socket.gethostname())
 	sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sck.bind((host,port))
 	sck.listen(1)
 	conn, adr = sck.accept()
+	
 	print ('Client Connected, IP : ', adr)
+	
 	while True:
-		data = conn.recv(1024)
-		if not data: break
-		conn.sendall(data)
-		print data
-		sck.close()
-	conn.close()
+		msg = raw_input('msg >')
+		try:
 
+			if msg == '/exit':
+				conn.close()
+			conn.send(msg)
+
+			data = conn.recv(1024)
+			if not data: break
+			conn.sendall(data)
+			print data
+		except:
+			conn.close()
+
+	'''
+	t = ThreadClass()
+	t.start(serverdata,(conn,adr))'''
+
+	
+	'''while True:
+		msg = raw_input('msg >')
+		try:
+			if msg == '/exit':
+				sck.close()
+			sck.send(msg)
+		except:
+			sck.close()
+	conn.close()'''
 
 def main(self):
 	option = raw_input("(1)Creative chatting room\n(2)Join the chatting room\nInput: ")
